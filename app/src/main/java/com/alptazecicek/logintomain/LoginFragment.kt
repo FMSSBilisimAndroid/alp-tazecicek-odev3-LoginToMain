c90af14c145c7568af9fd5a2746cbebbc3413faf
Atzcck <alptazecicek@gmail.com>
2022-09-11 22:58:02 +0300

    initial commit

---
+++ app/src/main/java/com/alptazecicek/logintomain/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.alptazecicek.logintomain
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.alptazecicek.logintomain.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    val email = "fmss@gmail.com"
+    val password = "1234"
+    private lateinit var fragmentLoginBinding: FragmentLoginBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        return fragmentLoginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentLoginBinding.logInBtn.setOnClickListener {
+            if (fragmentLoginBinding.emailEdittext.text.toString() == email
+                && fragmentLoginBinding.passwordEdittext.text.toString() == password )
+            findNavController().navigate(R.id.action_loginFragment_to_main_graph)
+            else Toast.makeText(requireContext(), "Wrong email and password!\nHint= $email $password", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/alptazecicek/logintomain/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.alptazecicek.logintomain
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.alptazecicek.logintomain.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var fragmentMainBinding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentMainBinding = FragmentMainBinding.inflate(inflater)
+        return fragmentMainBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentMainBinding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+}
